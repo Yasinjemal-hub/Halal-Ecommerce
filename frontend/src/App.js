f3e77aec97dad33b1381bd8d82e6690eea9bac97
Yasingemale-hub <yasin.ugr-8504-16@aau.edu.et>
2026-02-18 03:00:34 -0800

    modify on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React  from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+   <h1>Halal E-Commerce App</h1>
   );
 }
 
